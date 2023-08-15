8e399d524d1012196f39766515d40fcdf48e949f
MicaioValente <micaiovalente@gmail.com>
2023-08-15 13:07:45 -0300

    init

---
+++ dist/components/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+const Button = () => (React.createElement(View, null,
+    React.createElement(Text, null, "Meu Componente")));
+export default Button;
